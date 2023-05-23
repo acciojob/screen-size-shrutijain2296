cb39d3c3b53b9471dc345e462420e1aa2e28ae5f
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-23 11:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-const divTag = document.createElement("div")
-divTag.id = "sizeInfo"
-const sizeElement = document.createElement("h1")
+const divTag = document.createElement("div");
+divTag.Id = "sizeInfo";
+const sizeElement = document.createElement("h1");
 sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
 divTag.append(sizeElement);
 window.onresize = function(){
